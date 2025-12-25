cb7c1b48f76d3d1c1a589a5628a6f25bdb1dc3bb
haowen737 <haowen737@gmail.com>
2025-12-25 14:35:08 +0800

    Add Swift Package manifest

---
+++ Package.swift
@@ -0,0 +1,41 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+    name: "KTVHTTPCache",
+    platforms: [
+        .iOS(.v12),
+        .tvOS(.v12),
+        .macOS(.v10_15),
+    ],
+    products: [
+        .library(
+            name: "KTVHTTPCache",
+            targets: ["KTVHTTPCache"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "CocoaAsyncSocket",
+            path: "Vendors/CocoaAsyncSocket",
+            publicHeadersPath: "."
+        ),
+        .target(
+            name: "KTVHTTPCache",
+            dependencies: ["CocoaAsyncSocket"],
+            path: "KTVHTTPCache",
+            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath("Classes/KTVHCCommon"),
+                .headerSearchPath("Classes/KTVHCDataStorage"),
+                .headerSearchPath("Classes/KTVHCHTTPServer"),
+                .headerSearchPath("Classes/KTVHCDownload"),
+                .headerSearchPath("Classes/KTVHCTools"),
+                .headerSearchPath("CocoaHTTPServer"),
+                .headerSearchPath("CocoaHTTPServer/Categories"),
+                .headerSearchPath("CocoaHTTPServer/Mime"),
+                .headerSearchPath("CocoaHTTPServer/Responses"),
+            ]
+        ),
+    ]
+)
